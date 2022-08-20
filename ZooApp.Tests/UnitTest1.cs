c6ec30b4859cef7eea24704fbb019480ce9f2e6e
Dmitry <dmitrijtolstyh97@gmail.com>
2022-08-20 15:43:36 +0800

    Add project files.

---
+++ ZooApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ZooApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/ZooLib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ZooLib
+{
+    public class Class1
+    {
+
+    }
+}

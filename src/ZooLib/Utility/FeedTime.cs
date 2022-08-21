c746934c81791979925dde63f9be75f1083bfd8d
Dmitry <dmitrijtolstyh97@gmail.com>
2022-08-21 17:51:20 +0800

    feat: add feedtime

---
+++ src/ZooLib/Utility/FeedTime.cs
@@ -0,0 +1,16 @@
+﻿using ZooLib.Employees;
+
+namespace ZooLib.Utility
+{
+    public class FeedTime
+    {
+        public readonly DateTime DateTime;
+        public readonly ZooKeeper ZooKeeper;
+
+        public FeedTime(DateTime dateTime, ZooKeeper zooKeeper)
+        {
+            DateTime = dateTime;
+            ZooKeeper = zooKeeper;
+        }
+    }
+}
